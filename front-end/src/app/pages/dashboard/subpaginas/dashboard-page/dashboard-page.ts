2bd81dc56bf43121f46bb9578fb1f159f8c7c52b
MessiasTeixeira <messias4910@gmail.com>
2026-05-20 17:22:16 -0300

    feat: commit inicial com aplicação do dashboard funcionando

--- front-end/src/app/pages/dashboard/subpaginas/dashboard-page/dashboard-page.ts
+++ front-end/src/app/pages/dashboard/subpaginas/dashboard-page/dashboard-page.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PetService } from '../../../../services/pet/pet';
+import { ProximosAgendamentosService } from '../../../../services/proximos-agendamentos/proximos-agendamentos';
+import { HistoricoAnimalService } from '../../../../services/historico-animal/historico-animal';
+import { Cards } from '../../../../components/cards/cards';
+import { HistoricoAnimal } from '../../../../components/historico-animal/historico-animal';
+import { ProximosAgendamentos } from '../../../../components/proximos-agendamentos/proximos-agendamentos';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [Cards, HistoricoAnimal, ProximosAgendamentos],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.css',
 })
-export class DashboardPage {}
+
+export class DashboardPage {
+  private petService = inject(PetService);
+  pets = this.petService.pets();
+
+  private proximosAgendamentosService = inject(ProximosAgendamentosService);
+  agendamentos = this.proximosAgendamentosService.proximosAgendamentos()
+
+  private historicoAnimalService = inject(HistoricoAnimalService);
+  historicos = this.historicoAnimalService.historicoAnimal();
+}
