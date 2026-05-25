59a129d1dd51bd2fd8eaf5ca61c3f56b6e1a23ec
Caio Mayan <caiotensai@gmail.com>
2026-05-24 23:40:43 -0300

    feat: add 404 page and correct icons for material-icons

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -5,15 +5,17 @@ import { Cadastro } from './pages/cadastro/cadastro';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { dashboardRoutes } from './pages/dashboard/dashboard.routes';
 import { PrecoComponent } from './pages/home/subpaginas/preco/preco';
+import { NotFoundPage } from './pages/not-found/not-found';
 
 export const routes: Routes = [
-  { path: '', component: Home, title: 'Home' },
-  { path: 'login', component: Login, title: 'Login' },
-  { path: 'cadastro', component: Cadastro, title: 'Cadastro' },
-  { path: 'preco', component: PrecoComponent, title: 'Preços' },
+  { path: '', component: Home, title: 'AgendaPet - Gestão de Clínicas Veterinárias e Pet Shops' },
+  { path: 'login', component: Login, title: 'AgendaPet - Login' },
+  { path: 'cadastro', component: Cadastro, title: 'AgendaPet - Cadastro' },
+  { path: 'preco', component: PrecoComponent, title: 'AgendaPet - Preços' },
   {
     path: 'dashboard',
     component: Dashboard,
     children: dashboardRoutes,
   },
+  { path: '**', component: NotFoundPage, title: 'AgendaPet - 404' },
 ];
